2d83fef768a4212eb582c02fe09bacf6294e2dd6
Dominic Fargas Jr. <dcfargas@up.edu.ph>
2025-12-19 18:28:46 +0800

    better buttons

--- lib/ReactViews/Notification/DialogMeasureContent.tsx
+++ lib/ReactViews/Notification/DialogMeasureContent.tsx
@@ -3,6 +3,7 @@ import { observer } from "mobx-react";
 import MapInteractionMode from "../../Models/MapInteractionMode";
 import ViewState from "../../ReactViewModels/ViewState";
 import parseCustomHtmlToReact from "../Custom/parseCustomHtmlToReact";
+import { RawButton } from "../../Styled/Button";
 
 interface DialogMeasureContentProps {
   message: string;
@@ -13,17 +14,58 @@ interface DialogMeasureContentProps {
 const DialogMeasureContent: React.FC<DialogMeasureContentProps> = observer(
   ({ message, interactionMode, viewState }) => {
     return (
-      <div>
+      <>
         {parseCustomHtmlToReact(message)}
+
         {interactionMode.isMeasurementMode && (
-          <div style={{ marginTop: "10px", display: "flex", gap: "10px" }}>
-            <button onClick={() => viewState.setPrintWindow(window.open())}>
+          <div
+            css={`
+              display: flex;
+              justify-content: center;
+              gap: 12px;
+              margin-top: 16px;
+            `}
+          >
+            <RawButton
+              onClick={() => viewState.setPrintWindow(window.open())}
+              css={`
+                min-width: 90px;
+                height: 38px;
+                padding: 0 18px;
+                border-radius: 12px;
+
+                border: 2px solid #433977;
+                background: white;
+                color: #433977;
+
+                font-size: 15px;
+                font-weight: 500;
+              `}
+            >
               Print
-            </button>
-            <button onClick={interactionMode.onCancel}>Done</button>
+            </RawButton>
+
+            <RawButton
+              onClick={interactionMode.onCancel}
+              css={`
+                min-width: 90px;
+                height: 38px;
+                padding: 0 18px;
+                border-radius: 12px;
+
+                background: #433977;
+                color: white;
+                border: none;
+
+                font-size: 15px;
+                font-weight: 500;
+              `}
+            >
+              Done
+            </RawButton>
           </div>
         )}
-      </div>
+      </>
     );
   }
 );
